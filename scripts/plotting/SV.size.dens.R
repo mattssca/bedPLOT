eb29f4242e71e7ecd6c678a079c0067fa6a3c926
Adam <mattsada@gmail.com>
2020-05-25 16:01:10 -0700

    fix plot

--- scripts/plotting/SV.size.dens.R
+++ scripts/plotting/SV.size.dens.R
@@ -23,4 +23,4 @@ SV.calls = read.table(file = input.file, header = FALSE, sep = "\t", skip = 1)
 SV.size.dens = ggplot(SV.calls, aes(x = V4)) + 
   labs(title = plot.title, subtitle = plot.subtitle, x = x.axis.name, y = y.axis.name) +
   geom_density(alpha = 0.7, color = "black", fill = dataset1.colour, adjust = 1, position = "stack") +
-  scale_x_continuous(limits = c(0, 1300000), breaks = c(0, 200000, 400000, 600000, 800000, 1000000, 1200000, 1400000))
+  scale_x_continuous(limits = c(0, 1300000), breaks = c(0, 200000, 400000, 600000, 800000, 1000000, 1200000))
