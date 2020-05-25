a27d543bf72b9378dd88e0ed4c0038abf862a4f0
Adam <mattsada@gmail.com>
2020-05-25 15:22:28 -0700

    adding violine plot

--- README.md
+++ README.md
@@ -13,10 +13,14 @@ Script `SV.size.dens.R` takes the total number of SVs pressent in the input bed 
 
 ![SV.size.dens.R](https://github.com/mattsada/invPLOT/blob/master/example_figures/SV.size.dens.R.png)
 
-### 02 - Size distribution boxplot
+### 02 - Size distribution box plot
 Script `SV.size.box.R` plots the size distributions of SVs pressent in input bed file. Outliers, first and fourth quantile as well as mean inversion size are visualized in the same plot.
 
 ![SV.dens.box.R](https://github.com/mattsada/invPLOT/blob/master/example_figures/SV.size.box.R.png)
 
+###03 - Size distribution violine plot
+Script `SV.size.violine.R` plots SV size distribution as a un-trimmed violine plot, annotating quantiles as well as SV size density.
+
+![SV.size.violine.R]()
 
 
---
+++ scripts/plotting/SV.size.violine.R
@@ -0,0 +1,34 @@
+# load required package
+library(ggplot2)
+library(dplyr)
+
+# set path to bed file to be plotted (for madditional callsets add more lines to match number of callsets)
+input.file = "testdata/inv.calls.bed"
+
+# set titles, subtitles, axis, scales etc.
+plot.title = "Inversion size"
+plot.subtitle = "HG0005.GRCh38"
+y.axis.name = "Size (bp)"
+callset.name = "Strand.seq.invertR"
+
+# define colours for fill (if more than two datasets are to be plotted, add more colours accordingly)
+dataset1.colour = "#CA8BAB"
+
+# read SV callset into R data frame (skips first Header line)
+SV.calls = read.table(file = input.file, header = FALSE, sep = "\t", skip = 1)
+
+# add dataset variable to dataframe
+namevector <- c("V5")
+SV.calls[,namevector] <- callset.name
+
+#convert variable from numeric to factor
+SV.calls$V5 = as.factor(SV.calls$V5)
+SV.calls$V1 = as.factor(SV.calls$V1)
+
+########################################################plotting########################################################
+
+vplot <- ggplot(SV.calls, aes(x = V5, y = V4, fill = V5)) + 
+  stat_summary(fun = mean, geom = "point", shape = 23, size = 3) +
+  labs(title = plot.title, subtitle = plot.subtitle, x = "", y = y.axis.name, fill = "Data" ) +
+  theme(legend.position = "none") +
+  geom_violin(trim = FALSE, scale = "width", draw_quantiles = c(0.25, 0.5, 0.75), fill = dataset1.colour)
